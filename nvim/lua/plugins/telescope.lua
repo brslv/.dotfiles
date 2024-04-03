d3a2e77aaf1ce0a0af3d5b0d295471953242ba9c
Borislav Grigorov <borislav.b.grigorov@gmail.com>
2024-04-03 17:03:15 +0300

    telescope dropdown fixed

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -21,6 +21,23 @@ return {
           },
         },
       },
+      pickers = {
+        find_files = {
+          theme = "dropdown",
+        },
+        oldfiles = {
+          theme = "dropdown",
+        },
+        live_grep = {
+          theme = "dropdown",
+        },
+        grep_string = {
+          theme = "dropdown",
+        },
+        resume = {
+          theme = "dropdown",
+        },
+      },
     })
 
     telescope.load_extension("fzf")
@@ -28,7 +45,7 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files theme=dropdown<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
