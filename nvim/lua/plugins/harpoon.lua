f4efeab9e1e3d5c49259abef5eb5ce1b195cd2b5
Borislav Grigorov <borislav.b.grigorov@gmail.com>
2024-04-16 14:57:10 +0300

    change harpoon mappings

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -9,28 +9,28 @@ return {
     harpoon:setup()
     -- REQUIRED
 
-    vim.keymap.set("n", "<leader>ad", function()
+    vim.keymap.set("n", "<leader>dd", function()
       harpoon:list():add()
     end)
-    vim.keymap.set("n", "<leader>ah", function()
+    vim.keymap.set("n", "<leader>dh", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
-    vim.keymap.set("n", "<leader>aw", function()
+    vim.keymap.set("n", "<leader>dw", function()
       harpoon:list():select(1)
     end)
-    vim.keymap.set("n", "<leader>ae", function()
+    vim.keymap.set("n", "<leader>de", function()
       harpoon:list():select(2)
     end)
-    vim.keymap.set("n", "<leader>as", function()
+    vim.keymap.set("n", "<leader>ds", function()
       harpoon:list():select(3)
     end)
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<leader>ap", function()
+    vim.keymap.set("n", "<leader>dp", function()
       harpoon:list():prev()
     end)
-    vim.keymap.set("n", "<leader>an", function()
+    vim.keymap.set("n", "<leader>dn", function()
       harpoon:list():next()
     end)
   end,
